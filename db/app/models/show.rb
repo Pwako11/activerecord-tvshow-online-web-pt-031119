6795af8e0ffe89f78bf262d5132830c43465b04f
Patrick A <akolo@hotmail.com>
2019-08-18 12:57:26 +0000

    Automatically backed up by Learn

--- db/app/models/show.rb
+++ db/app/models/show.rb
@@ -1,3 +1,30 @@
 class Show < ActiveRecord::Base
-  include OneHelper
+  
+  def highest_rating 
+  Show.maximum(:rating)
+  end 
+  
+  def most_popular_show
+    self.where("rating = ?", self.highest_rating).first
+  end 
+
+  def lowest_rating
+    Show.minimum(:rating)
+  end
+  
+  def least_popular_show
+   Show.where("rating > ?", self.Lowest_rating).fisrt
+  end 
+  
+  def ratings_sum
+    Show.sum(:rating)
+  end 
+  
+  def popular_shows
+    Show.where("rating>=?", 5)
+  end 
+  
+  def shows_by_alphabetical_order
+  Show.order(:name)
+  end 
 end 
